0fadbf3905bd9550986a9499ae5853698f565210
melaveetha <retromeras@ya.ru>
2022-10-17 15:58:21 +0300

    Add [lazy load routes] global

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Main from './Main.vue'
-import Second from './Second.vue'
-import NotFound from './NotFound.vue'
 
 const routes = [
-    {path: '/', name: 'main', component: Main, alias: ["/main"]},
-    {path: '/second', name: 'second', component: Second},
-    {path: '/:uri(.*)*', name: 'notFound', component: NotFound},
+    {path: '/', name: 'main', component: () => import('./Main.vue'), alias: ["/main"]},
+    {path: '/second', name: 'second', component: () => import('./Second.vue')},
+    {path: '/:uri(.*)*', name: 'notFound', component: () => import('./NotFound.vue')},
 ]
+
 
 const router = createRouter({
     history: createWebHistory(),
